fca707bf38abc4934ff455e47fb91da5f5440180
giantmons <santosleuan@gmail.com>
2024-12-22 14:16:06 +0800

    added tab name

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Certifications from "./pages/Certifications";
 import Projects from "./pages/Projects";
+import { Helmet } from 'react-helmet';
 
 
 
 function App() {
 
   return (
+    <>
+    <Helmet>
+        <title>Leuan Santos</title>
+        <link rel="icon" type="image/png" href="/world.png" />
+    </Helmet>
     <Router>
       <Routes>
         <Route path="/" element={ <Home/> }/>
@@ -16,6 +22,7 @@ function App() {
         <Route path="/Projects" element={ <Projects/> }/>
       </Routes>
     </Router>
+    </>
   );
 }
 
