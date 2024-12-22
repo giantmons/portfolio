ce279f20f0b5b26d7c9a4cc8cd537341e75c46db
giantmons <santosleuan@gmail.com>
2024-12-22 11:56:17 +0800

    finalization of certifications page

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -1,28 +1,101 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Certifications = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+      easing: 'ease-in-out'
+    });
+  }, []);
+
+  const handleGoBack = () => {
+    navigate(-1);
+  }
+
   return (
     <main className="flex flex-col min-h-screen dark:bg-black font-poppins">
-      <div className="flex justify-center mt-8">
-        <h1 className="text-2xl font-semibold dark:text-white">All Certifications</h1>
+      <div 
+        className="flex justify-start gap-6 mt-8 w-11/12 mx-auto md:w-8/12"
+        data-aos="fade-up"
+      >
+        <button 
+          className='text-sm dark:text-white' 
+          onClick={handleGoBack}
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
+          Back to Home
+        </button>
+        <h1 
+          className="text-2xl font-semibold dark:text-white self-center"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
+          All Certifications
+        </h1>
       </div>
-      <div className="mt-8 grid grid-cols-1 gap-y-2 gap-x-2 w-11/12 md:grid-cols-2 px-4 md:w-7/12 mx-auto">
-        <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111">
-          <h1 className="text-sm font-semibold mb-2 dark:text-white">CCNAv7: Introduction to Networks</h1>
-          <p className="text-sm dark:text-white">Issued Date: Feb 01, 2023</p>
-        </div>
-        <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111">
-          <h1 className="text-sm font-semibold mb-2 dark:text-white">CCNAv7: Introduction to Networks</h1>
-          <p className="text-sm dark:text-white">Issued Date: Feb 01, 2023</p>
-        </div>
-        <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111">
-          <h1 className="text-sm font-semibold mb-2 dark:text-white">CCNAv7: Introduction to Networks</h1>
-          <p className="text-sm dark:text-white">Issued Date: Feb 01, 2023</p>
-        </div>
-        <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111">
-          <h1 className="text-sm font-semibold mb-2 dark:text-white">CCNAv7: Introduction to Networks</h1>
-          <p className="text-sm dark:text-white">Issued Date: Feb 01, 2023</p>
-        </div>
+
+      <div className="mt-8 grid grid-cols-1 gap-y-2 gap-x-2 w-11/12 md:grid-cols-2 px-4 md:w-8/12 mx-auto">
+        <a 
+          href="https://www.credly.com/badges/d2528bda-f486-4e9a-9196-53a3f4b2bc89"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111 hover:translate-y-[-2px] transition-transform duration-300 cursor-pointer">
+            <h1 className="text-sm font-semibold mb-2 dark:text-white">CCNA: Introduction to Networks - CISCO</h1>
+            <p className="text-sm dark:text-white">Issued Date: Feb 01, 2023</p>
+          </div>
+        </a>
+
+        <a 
+          href="https://www.credly.com/badges/79f2871d-700f-4d20-a2d3-e73a212f53c7"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111 hover:translate-y-[-2px] transition-transform duration-300 cursor-pointer">
+            <h1 className="text-sm font-semibold mb-2 dark:text-white">CCNA: Enterprise Networking, Security, and Automation - CISCO</h1>
+            <p className="text-sm dark:text-white">Issued Date: Feb 12, 2024</p>
+          </div>
+        </a>
+
+        <a 
+          href="https://www.credly.com/badges/2eb7b68e-80ec-4f2f-8bb7-c877644064da"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111 hover:translate-y-[-2px] transition-transform duration-300 cursor-pointer">
+            <h1 className="text-sm font-semibold mb-2 dark:text-white">Cybersecurity Essentials - CISCO</h1>
+            <p className="text-sm dark:text-white">Issued Date: May 18, 2024</p>
+          </div>
+        </a>
+
+        <a 
+          href="https://www.credly.com/badges/a7d6f4ee-fc35-420a-91f6-c27325faf019"
+          data-aos="fade-up"
+          data-aos-delay="400"
+        >
+          <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111 hover:translate-y-[-2px] transition-transform duration-300 cursor-pointer">
+            <h1 className="text-sm font-semibold mb-2 dark:text-white">Introduction to Cloud - AWS Academy</h1>
+            <p className="text-sm dark:text-white">Issued Date: May 18, 2024</p>
+          </div>
+        </a>
+
+        <a 
+          href="https://www.credly.com/badges/d5ac4006-cd5e-4e34-8cc5-ec117233e194"
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
+          <div className="border rounded-lg p-4 dark:bg-dark-111 dark:border-gray-111 hover:translate-y-[-2px] transition-transform duration-300 cursor-pointer">
+            <h1 className="text-sm font-semibold mb-2 dark:text-white">Cloud Security Foundations - AWS Academy</h1>
+            <p className="text-sm dark:text-white">Issued Date: Dec 21, 2024</p>
+          </div>
+        </a>
       </div>
     </main>
   );
