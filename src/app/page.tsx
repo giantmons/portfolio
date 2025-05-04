ffc0229ea7139fb4f3f9b653678c3cdce8d4a3bf
giantmons <santosleuan@gmail.com>
2025-05-04 12:59:24 +0800

    Added lanyard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
           {/* ABOUT ME CONTAINER */}
           <FadeInOnScroll className="bg-white dark:bg-[#111111] dark:text-white sm:col-span-4 row-span-3 border border-gray-200 dark:border-[#333333] p-5 rounded-lg transition hover:shadow-lg">
             <h1 className="font-semibold text-lg flex gap-2 items-center"><PersonStanding size={18} className="opacity-40" /> About</h1>
-            <p className="text-sm mt-1 leading-6">I'm an aspiring software engineer with a passion for building responsive, accessible,
+            <p className="text-sm mt-1 leading-6">I'm an aspiring software developer with a passion for building responsive, accessible,
               and user-friendly web interfaces. I love working with modern design principles, focusing on performance, and making
               sure every user has a smooth experience. I'm experienced in creating interactive features, making sure websites look
               great on any device, and following best practices in UI/UX design.
