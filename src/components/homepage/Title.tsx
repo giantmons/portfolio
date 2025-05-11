1b390d92ea10031cac36e298a989d505ba185540
giantmons <santosleuan@gmail.com>
2025-05-11 12:46:01 +0800

    Fixed Largest Contentful Paint

--- src/components/homepage/Title.tsx
+++ src/components/homepage/Title.tsx
@@ -1,14 +1,13 @@
-"use client"
+
 import FadeInOnScroll from "../fade-on-scroll/FadeInOnScroll";
 import Stack from "../stack/stack";
-import { useTheme } from "@/components/theme/theme-context";
 import onePic from "@/assets/stack/one.png"
 import twoPic from "@/assets/stack/two.webp"
 import threePic from "@/assets/stack/three.webp"
 import fourPic from "@/assets/stack/four.png"
+import ThemeController from "../theme/theme-controller";
 
 const Title = () => {
-      const { theme, toggleTheme } = useTheme();
     
       const images: { id: number; img: string }[] = [
         { id: 1, img: fourPic.src },
@@ -25,20 +24,7 @@ const Title = () => {
                 <p className="text-sm leading-6 text-center xl:text-left">Frontend Developer. I love bringing my ideas to code,
                     but not when bugs get creative too.
                 </p>
-                <label className="toggle theme-controller cursor-pointer h-8 w-14 mt-2">
-                    <input
-                        type="checkbox"
-                        checked={theme === "dark"}
-                        onChange={toggleTheme}
-                        className="hidden"
-                    />
-
-                    {/* Sun Icon for Light Mode */}
-                    <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
-
-                    {/* Moon Icon for Dark Mode */}
-                    <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
-                </label>
+                <ThemeController />
 
             </div>
 
